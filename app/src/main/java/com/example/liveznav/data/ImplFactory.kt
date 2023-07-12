f4ed9feb7cef1dfe81eeee6cbbe4a7d9e58a774a
Betz <silverbez@gmail.com>
2023-07-12 17:58:23 +0300

    also http call

--- app/src/main/java/com/example/liveznav/data/ImplFactory.kt
+++ app/src/main/java/com/example/liveznav/data/ImplFactory.kt
@@ -2,6 +2,9 @@ package com.example.liveznav.data
 
 import com.example.liveznav.utils.Consts
 import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.net.URL
 
 object ImplFactory {
 
@@ -10,8 +13,8 @@ object ImplFactory {
     }
 
     private fun create(): ApiImpl {
-        return FakeArticlesApi()
-//        return ArticlesApi()
+//        return FakeArticlesApi()
+        return ArticlesApi()
     }
 
 }
@@ -23,9 +26,33 @@ internal interface ApiImpl {
 
 private class ArticlesApi : ApiImpl {
 
+    val client = OkHttpClient()
+
+
     override suspend fun getItems(name: String): CountriesPerName {
-        return CountriesPerName(listOf(), name)
+        val url = Consts.baseUrl + name
+        val jsonString = getRequest(url)
+        val countriesPerName = Gson().fromJson(jsonString, CountriesPerName::class.java)
+        return countriesPerName
+
     }
+
+    private fun getRequest(sUrl: String): String? {
+        var result: String? = null
+        try {
+            // Create URL
+            val url = URL(sUrl)
+            // Build request
+            val request = Request.Builder().url(url).build()
+            // Execute request
+            val response = client.newCall(request).execute()
+            result = response.body?.string()
+        } catch (err: Error) {
+            print("Error when executing get request: " + err.localizedMessage)
+        }
+        return result
+    }
+
 }
 
 private class FakeArticlesApi : ApiImpl {
@@ -35,7 +62,6 @@ private class FakeArticlesApi : ApiImpl {
         val jsonString = Consts.jsonRaw
 
         val countriesPerName = Gson().fromJson(jsonString, CountriesPerName::class.java)
-
         return countriesPerName
     }
 
--- app/src/main/java/com/example/liveznav/utils/Consts.kt
+++ app/src/main/java/com/example/liveznav/utils/Consts.kt
@@ -5,5 +5,7 @@ class Consts {
         val jsonRaw = """
             {"name":"tal","country":[{"country_id":"IL","probability":0.927},{"country_id":"PA","probability":0.007},{"country_id":"TH","probability":0.006},{"country_id":"CA","probability":0.003},{"country_id":"AU","probability":0.003}]}
         """.trimIndent()
+
+        const val baseUrl = "https://api.nationalize.io/?name="
     }
 }
